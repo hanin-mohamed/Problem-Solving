f66a5d947ca1e8618c2fe15e9b8e542519710bb8
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-09-16 15:59:35 +0300

    Meduim Array& Hash Problem

---
+++ NeetCode/src/Medium/AnagramGroups.java
@@ -0,0 +1,42 @@
+package Medium;
+
+import java.util.*;
+
+public class AnagramGroups {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>>list = new ArrayList<>();
+        Map<String,List<String>> map=new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String st = new String(chars);
+            List<String> acc=map.getOrDefault(st,new ArrayList<>());
+            acc.add(str);
+            map.put(st,acc);
+            }
+       list.addAll(map.values());
+        return list;
+    }
+    //Anthor Solution Time 6ms ,Memory 48.29 MB
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        List<List<String>> list = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sort = new String(chars);
+            List<String> acc = map.get(sort);
+            if (acc==null){
+                acc=new ArrayList<>();
+                map.put(sort,acc);
+            }
+            acc.add(str);
+        }
+        list.addAll(map.values());
+        return list;
+    }
+    public static void main(String args[]){
+        AnagramGroups anagramGroups= new AnagramGroups();
+        System.out.println(anagramGroups.groupAnagrams(new  String[]{"eat","tea","tan","ate","nat","bat"}));
+    }
+}
