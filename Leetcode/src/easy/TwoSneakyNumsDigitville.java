e2d5b47f6e8720670082adcb498485c817aa346c
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-09-16 17:09:59 +0300

    Anthor Solution

--- Leetcode/src/easy/TwoSneakyNumsDigitville.java
+++ Leetcode/src/easy/TwoSneakyNumsDigitville.java
@@ -1,22 +1,34 @@
 package easy;
 
 import java.sql.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSneakyNumsDigitville {
     public int[] getSneakyNumbers(int[] nums) {
-        Map<Integer,Integer> map= new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        int cnt=0;
+        int cnt = 0;
         int[] sneaky = new int[2];
-        for (Map.Entry<Integer,Integer>entry:map.entrySet()) {
-            if (entry.getValue() == 2) {
-                sneaky[cnt] = entry.getKey();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                sneaky[cnt] = nums[i];
                 cnt++;
             }
+        }
+        return sneaky;
+    }
+    //Anthor Solution
+    public int[] getSneakyNumbers2(int[] nums) {
+        int cnt = 0;
+        int[] sneaky = new int[2];
+        Map<Integer,Integer> map= new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])){
+                sneaky[cnt]= nums[i];
+                cnt++;
+            }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         return sneaky;
     }
