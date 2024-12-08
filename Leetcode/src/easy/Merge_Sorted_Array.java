48a39d21fbe407caf0847daa5585a7726b0da1f7
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-12-09 00:13:27 +0200

    Merge Sorted Array

--- Leetcode/src/easy/Merge_Sorted_Array.java
+++ Leetcode/src/easy/Merge_Sorted_Array.java
@@ -2,5 +2,20 @@ package easy;
 
 public class Merge_Sorted_Array {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l=m+n-1,r1=m-1,r2=n-1;
+        while (r1>=0 && r2>=0) {
+            if (nums1[r1]>nums2[r2]){
+                nums1[l--]=nums1[r1--];
+            }else {
+                nums1[l--]=nums2[r2--];
+            }
+        }
+        while (r2>=0){
+            nums1[l--]=nums2[r2--];
+        }
+    }
+    public static void main(String[] args) {
+        Merge_Sorted_Array m = new Merge_Sorted_Array();
+        m.merge(new int[]{1,2,3,0,0,0},3,new int[]{2,5,6},3);
     }
 }
---
+++ Leetcode/src/meduim/Multiply_Strings.java
@@ -0,0 +1,11 @@
+//package meduim;
+//
+//import static java.lang.Math.pow;
+//
+//public class Multiply_Strings {
+//    public String multiply(String num1, String num2) {
+//    public static void main(String[] args) {
+//        Multiply_Strings s = new Multiply_Strings();
+//        System.out.println(s.multiply("123", "456"));
+//    }
+//}
